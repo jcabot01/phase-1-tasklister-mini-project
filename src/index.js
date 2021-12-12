f8cab26957d7e1caade319c5ae60e53faa56dd42
JonCabot <jfc0053@gmail.com>
2021-12-12 16:28:54 -0600

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new.value)
+    form.reset()
+  })
 });
+
+function buildToDo(todo) {
+  console.log(todo) 
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  let input = document.createElement('input')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo } `
+  p.appendChild(btn)
+  p.appendChild(input)
+  input.textContent = "notes"
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
